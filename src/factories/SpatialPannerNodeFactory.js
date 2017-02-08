89ecfc56e57032401622a2cd590e84d7c7e8a55b
mohayonao <mohayonao@gmail.com>
2017-02-08 15:42:40 +0900

    add type check for constructor of PannerNode

--- src/factories/SpatialPannerNodeFactory.js
+++ src/factories/SpatialPannerNodeFactory.js
@@ -27,8 +27,43 @@ function create(api, AudioNode) {
      * @protected - use 'audioContext.createSpatialPanner()' instead
      * @param {BaseAudioContext} context
      * @param {object} opts
+     * @param {PanningModelType} opts.panningModel
+     * @param {DistanceModelType} opts.distanceModel
+     * @param {finite} opts.refDistance
+     * @param {finite} opts.maxDistance
+     * @param {finite} opts.rolloffFactor
+     * @param {finite} opts.coneInnerAngle
+     * @param {finite} opts.coneOuterAngle
+     * @param {finite} opts.coneOuterGain
+     * @param {finite} opts.positionX
+     * @param {finite} opts.positionY
+     * @param {finite} opts.positionZ
+     * @param {finite} opts.orientationX
+     * @param {finite} opts.orientationY
+     * @param {finite} opts.orientationZ
      */
     constructor(context, opts = {}) {
+      const panningModel = defaults(opts.panningModel, DEFAULT_PANNING_MODEL);
+      const distanceModel = defaults(opts.distanceModel, DEFAULT_DISTANCE_MODEL);
+      const refDistance = defaults(opts.refDistance, DEFAULT_REF_DISTANCE);
+      const maxDistance = defaults(opts.maxDistance, DEFAULT_MAX_DISTANCE);
+      const rolloffFactor = defaults(opts.rolloffFactor, DEFAULT_ROLLOFF_FACTOR);
+      const coneInnerAngle = defaults(opts.coneInnerAngle, DEFAULT_CONE_INNER_ANGLE);
+      const coneOuterAngle = defaults(opts.coneOuterAngle, DEFAULT_CONE_OUTER_ANGLE);
+      const coneOuterGain = defaults(opts.coneOuterGain, DEFAULT_CONE_OUTER_GAIN);
+      const positionX = defaults(opts.positionX, DEFAULT_POSITION_X);
+      const positionY = defaults(opts.positionY, DEFAULT_POSITION_Y);
+      const positionZ = defaults(opts.positionZ, DEFAULT_POSITION_Z);
+      const orientationX = defaults(opts.orientationX, DEFAULT_ORIENTATION_X);
+      const orientationY = defaults(opts.orientationY, DEFAULT_ORIENTATION_Y);
+      const orientationZ = defaults(opts.orientationZ, DEFAULT_ORIENTATION_Z);
+
+      opts = Object.assign({}, opts, {
+        panningModel, distanceModel, refDistance, maxDistance,
+        rolloffFactor, coneInnerAngle, coneOuterAngle, coneOuterGain,
+        positionX, positionY, positionZ, orientationX, orientationY, orientationZ
+      });
+
       try { lock.unlock();
         super(context, opts, {
           inputs: [ 1 ],
@@ -177,47 +212,46 @@ function create(api, AudioNode) {
 }
 
 function initialize(api, context, opts) {
-  const panningModel = defaults(opts.panningModel, DEFAULT_PANNING_MODEL);
-  const distanceModel = defaults(opts.distanceModel, DEFAULT_DISTANCE_MODEL);
-  const refDistance = defaults(opts.refDistance, DEFAULT_REF_DISTANCE);
-  const maxDistance = defaults(opts.maxDistance, DEFAULT_MAX_DISTANCE);
-  const rolloffFactor = defaults(opts.rolloffFactor, DEFAULT_ROLLOFF_FACTOR);
-  const coneInnerAngle = defaults(opts.coneInnerAngle, DEFAULT_CONE_INNER_ANGLE);
-  const coneOuterAngle = defaults(opts.coneOuterAngle, DEFAULT_CONE_OUTER_ANGLE);
-  const coneOuterGain = defaults(opts.coneOuterGain, DEFAULT_CONE_OUTER_GAIN);
-  const positionX = defaults(opts.positionX, DEFAULT_POSITION_X);
-  const positionY = defaults(opts.positionY, DEFAULT_POSITION_Y);
-  const positionZ = defaults(opts.positionZ, DEFAULT_POSITION_Z);
-  const orientationX = defaults(opts.orientationX, DEFAULT_ORIENTATION_X);
-  const orientationY = defaults(opts.orientationY, DEFAULT_ORIENTATION_Y);
-  const orientationZ = defaults(opts.orientationZ, DEFAULT_ORIENTATION_Z);
-
-  this._.panningModel = panningModel;
-  this._.distanceModel = distanceModel;
-  this._.refDistance = refDistance;
-  this._.maxDistance = maxDistance;
-  this._.rolloffFactor = rolloffFactor;
-  this._.coneInnerAngle = coneInnerAngle;
-  this._.coneOuterAngle = coneOuterAngle;
-  this._.coneOuterGain = coneOuterGain;
+  this._.panningModel = opts.panningModel;
+  this._.distanceModel = opts.distanceModel;
+  this._.refDistance = opts.refDistance;
+  this._.maxDistance = opts.maxDistance;
+  this._.rolloffFactor = opts.rolloffFactor;
+  this._.coneInnerAngle = opts.coneInnerAngle;
+  this._.coneOuterAngle = opts.coneOuterAngle;
+  this._.coneOuterGain = opts.coneOuterGain;
   this._.positionX = new api.AudioParam(context, {
-    name: "Panner.positionX", defaultValue: DEFAULT_POSITION_X, value: positionX
+    name: "Panner.positionX", defaultValue: DEFAULT_POSITION_X, value: opts.positionX
   });
   this._.positionY = new api.AudioParam(context, {
-    name: "Panner.positionY", defaultValue: DEFAULT_POSITION_Y, value: positionY
+    name: "Panner.positionY", defaultValue: DEFAULT_POSITION_Y, value: opts.positionY
   });
   this._.positionZ = new api.AudioParam(context, {
-    name: "Panner.positionZ", defaultValue: DEFAULT_POSITION_Z, value: positionZ
+    name: "Panner.positionZ", defaultValue: DEFAULT_POSITION_Z, value: opts.positionZ
   });
   this._.orientationX = new api.AudioParam(context, {
-    name: "Panner.orientationX", defaultValue: DEFAULT_ORIENTATION_X, value: orientationX
+    name: "Panner.orientationX", defaultValue: DEFAULT_ORIENTATION_X, value: opts.orientationX
   });
   this._.orientationY = new api.AudioParam(context, {
-    name: "Panner.orientationY", defaultValue: DEFAULT_ORIENTATION_Y, value: orientationY
+    name: "Panner.orientationY", defaultValue: DEFAULT_ORIENTATION_Y, value: opts.orientationY
   });
   this._.orientationZ = new api.AudioParam(context, {
-    name: "Panner.orientationZ", defaultValue: DEFAULT_ORIENTATION_Z, value: orientationZ
+    name: "Panner.orientationZ", defaultValue: DEFAULT_ORIENTATION_Z, value: opts.orientationZ
   });
 }
 
 module.exports = { create, initialize };
+module.exports.DEFAULT_PANNING_MODEL = DEFAULT_PANNING_MODEL;
+module.exports.DEFAULT_DISTANCE_MODEL = DEFAULT_DISTANCE_MODEL;
+module.exports.DEFAULT_REF_DISTANCE = DEFAULT_REF_DISTANCE;
+module.exports.DEFAULT_MAX_DISTANCE = DEFAULT_MAX_DISTANCE;
+module.exports.DEFAULT_ROLLOFF_FACTOR = DEFAULT_ROLLOFF_FACTOR;
+module.exports.DEFAULT_CONE_INNER_ANGLE = DEFAULT_CONE_INNER_ANGLE;
+module.exports.DEFAULT_CONE_OUTER_ANGLE = DEFAULT_CONE_OUTER_ANGLE;
+module.exports.DEFAULT_CONE_OUTER_GAIN = DEFAULT_CONE_OUTER_GAIN;
+module.exports.DEFAULT_POSITION_X = DEFAULT_POSITION_X;
+module.exports.DEFAULT_POSITION_Y = DEFAULT_POSITION_Y;
+module.exports.DEFAULT_POSITION_Z = DEFAULT_POSITION_Z;
+module.exports.DEFAULT_ORIENTATION_X = DEFAULT_ORIENTATION_X;
+module.exports.DEFAULT_ORIENTATION_Y = DEFAULT_ORIENTATION_Y;
+module.exports.DEFAULT_ORIENTATION_Z = DEFAULT_ORIENTATION_Z;
